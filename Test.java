2fee02ddcc13820820979d02539527b56edc1355
carlos.zhang <625008739@qq.com>
2024-05-12 10:28:25 +0800

    test

--- Test.java
+++ Test.java
@@ -1,15 +1,14 @@
+
 public class Test {
     public static void main(String[] args) {
-        int a = 2;
         System.out.println("hello world");
-        System.out.println("abc=" + abc(a, 2));
+        System.out.println("5!=" + abc(5));
+        System.out.println("7!=" + abc(7));
     }
 
-    private static int abc(int x, int y) { //计算 x 的 y 次方
-        int result = 1;
-        for (int i = 0; i < y; i++) {
-            result *= x;
-        }
-        return result;
+    private static int abc(int x) {
+        System.out.println("方法abc传入参数：x=" + x);
+        //todo 练习：实现x的阶乘
+        return 0;
     }
 }
